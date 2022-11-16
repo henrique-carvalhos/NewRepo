cfeca70cea686118a7c48eb5eb225da871092cf7
henrique.carvalho <henrique.carvalho@HENRIQUE-CARVAL>
2022-11-16 12:49:51 -0300

    Add project files.

---
+++ CadAluno/LoginProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadAluno
+{
+    public class LoginProgram
+    {
+        //Atributos
+        private string Email;
+        private string ConfirmarEmail;
+        private string Senha;
+        private string ConfirmarSenha;
+
+        //Método construtor
+        public LoginProgram()
+        {
+        }
+
+        public LoginProgram(string email, string confirmarEmail, string senha, string confirmarSenha)
+        {
+            Email = email;
+            ConfirmarEmail = confirmarEmail;
+            Senha = senha;
+            ConfirmarSenha = confirmarSenha;
+        }
+
+        //Métodos Get e Set
+        public string getEmail()
+        {
+            return this.Email;
+        }
+        public void setEmail(string email)
+        {
+            this.Email = email;
+        }
+
+        public string getConfirmarEmail()
+        {
+            return this.ConfirmarEmail;
+        }
+        public void setConfirmarEmail(string confirmarEmail)
+        {
+            this.ConfirmarEmail = confirmarEmail;
+        }
+    }
+}
---
+++ CadAluno/TelaPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadAluno
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void alunoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TelaDeCadastro cadastroAluno = new TelaDeCadastro(); //Instância do objeto editor - Form2
+            cadastroAluno.MdiParent = this;    //Define o pai do filho, editor(filho) e pai(Form1)
+            cadastroAluno.Text = "Cadastro de Aluno";
+            cadastroAluno.Show();
+            cadastroAluno.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
